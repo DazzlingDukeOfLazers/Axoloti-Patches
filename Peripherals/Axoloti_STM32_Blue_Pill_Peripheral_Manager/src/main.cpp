8bdd428212e80967e53f28a4da2cfd85a60f8889
Daniel D Lindmark <daniel.dee@gmail.com>
2018-03-22 14:47:51 -0500

    OLED device found at 0x3C

--- Peripherals/Axoloti_STM32_Blue_Pill_Peripheral_Manager/src/main.cpp
+++ Peripherals/Axoloti_STM32_Blue_Pill_Peripheral_Manager/src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 //#include <TwoWire.h>
 
+#include <SoftWire.h>
+#include <SoftwireI2CTest.h>
+
+SoftWire SWire(PB6, PB7, SOFT_FAST);
+
+
 #define LED_BUILTIN PC13
 
 //TwoWire myWire(PB9, PB8, SOFT_FAST);
@@ -9,23 +15,19 @@ void setup()
 {
     // put your setup code here, to run once:
     pinMode(LED_BUILTIN, OUTPUT);
+
+    SWire.begin();
+    SoftWireI2CTest(SWire);
+
+    //OLEDInit();
+	//OLED_setstring();
+	//OLED_Sandbox();
+	//OLEDDisplay();
 }
-
-
-void hearbeat(int iDelay)
-{
-    // put your main code here, to run repeatedly:
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    delay(iDelay);
-
-    digitalWrite(LED_BUILTIN, LOW);
-
-    delay(iDelay);
-}
-
 
 void loop()
 {
-    hearbeat(3000);
+    //heartbeat(3000);
+    heartbeat(1000);
+
 }
